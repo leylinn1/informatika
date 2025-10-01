e5f0db2bcb9d779c0990b5aab80ef3699ec78a4d
leylinn1 <leylinn007@gmail.com>
2025-10-01 20:28:52 +0300

    Create main.cpp
    add.main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+#define M_PI 3.14159265358979323846
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    float x, y;
+    cout << "¬ведите x и y: ";
+    cin >> x >> y;
+
+    double R = sqrt(x * x + y * y) / pow(2, log2(x));
+
+    double S = x * tan(M_PI / 2);
+
+    cout << "R = " << R << endl;
+    cout << "S = " << S << endl;
+
+    double C = max(R, S);
+
+    cout << "C = " << C << endl;
+
+    return 0;
+}
